fa970aabd73775070eef1abf023873b00a30cb8d
seldadg <seldadg@gmail.com>
2019-06-28 17:15:40 +0300

    First commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+      this.client_id = 'e378caa3b00d665ed0ec';
+      this.client_secret = 'e443f84d11dc71b13d751596bb59e7201a4c6856';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
+    }
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        repos
+      }
+    }
+  }
